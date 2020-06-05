91c6e86a20efef4d7b8659eb41597ee48f676fe6
lightsing <light.tsing@gmail.com>
2020-06-05 16:39:51 +0800

    add doc for QuinineCache

--- src/main/kotlin/com/moumoux/quinine/QuinineCache.kt
+++ src/main/kotlin/com/moumoux/quinine/QuinineCache.kt
@@ -17,10 +17,24 @@ import java.util.concurrent.Executors
  * @param V the most general value type this builder will be able to create caches for.
  */
 interface QuinineCache<K : Any, V> {
+    /**
+     * Returns a current snapshot of this cache's cumulative statistics. All statistics are
+     * initialized to zero, and are monotonically increasing over the lifetime of the cache.
+     * <p>
+     * Due to the performance penalty of maintaining statistics, some implementations may not record
+     * the usage history immediately or at all.
+     *
+     * @return [QuinineCacheStats] snapshot instance
+     */
     val stats: QuinineCacheStats
-    val estimatedSize: Long
 
-    fun cleanUp()
+    /**
+     * Returns the approximate number of entries in this cache. The value returned is an estimate; the
+     * actual count may differ if there are concurrent insertions or removals, or if some entries are
+     * pending removal due to expiration or weak/soft reference collection. In the case of stale
+     * entries this inaccuracy can be mitigated by performing a [cleanUp] first.
+     */
+    val estimatedSize: Long
 
     /**
      * Discards any cached value for the [key]. The behavior of this operation is undefined for
@@ -29,7 +43,19 @@ interface QuinineCache<K : Any, V> {
      * @param key the key whose mapping is to be removed from the cache
      */
     fun invalidate(key: Any)
+
+    /**
+     * Discards any cached values for the [keys]. The behavior of this operation is undefined
+     * for an entry that is being loaded (or reloaded) and is otherwise not present.
+     *
+     * @param keys the keys whose associated values are to be removed
+     */
     fun invalidateAll(keys: Iterable<K>)
+
+    /**
+     * Discards all entries in the cache. The behavior of this operation is undefined for an entry
+     * that is being loaded (or reloaded) and is otherwise not present.
+     */
     fun invalidateAll()
 
     /**
@@ -99,6 +125,12 @@ interface QuinineCache<K : Any, V> {
      * @return the readonly mapping of keys to values for the specified keys found in this cache
      */
     suspend fun getAllPresent(keys: Iterable<K>): Map<K, V>
+
+    /**
+     * Performs any pending maintenance operations needed by the cache. Exactly which activities are
+     * performed -- if any -- is implementation-dependent.
+     */
+    fun cleanUp()
 }
 
 internal open class QuinineLocalCache<K : Any, V>(private val cache: Cache<K, Single<V>>) : QuinineCache<K, V> {
